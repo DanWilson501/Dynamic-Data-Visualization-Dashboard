f14e54a1a07164249a932a94a01b39e842de9787
Daniel Wilson <danwilson501@gmail.com>
2024-06-25 22:40:05 -0400

    SVG elements appended to visualize the line chart with x and y-axis within the 600x600 canvas. interactivity of each data point and tooltips to be added.

--- example.js
+++ example.js
@@ -25,3 +25,33 @@ d3.csv("data/mock_stock_data.csv").then(data => {
       drawChart(filteredData);
     }
   });
+
+   function drawChart(data) {
+    d3.select("#chart").html("");
+  
+    const margin = { top: 20, right: 30, bottom: 70, left: 50 },
+          width = 600 - margin.left - margin.right,
+          height = 600 - margin.top - margin.bottom;
+  
+    let svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+  
+    let x = d3.scaleTime().domain(d3.extent(data, d => d.date)).range([0, width]),
+        y = d3.scaleLinear().domain([0, d3.max(data, d => d.price)]).range([height, 0]),
+        line = d3.line().x(d => x(d.date)).y(d => y(d.price)),
+        color = d3.scaleOrdinal(d3.schemeCategory10),
+        nestedData = d3.group(data, d => d.stock);
+  
+    svg.append("g").attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(x).ticks(d3.timeDay.every(1)).tickFormat(d3.timeFormat("%Y-%m-%d")))
+      .selectAll("text").style("text-anchor", "end").attr("dx", "-.8em").attr("dy", ".15em").attr("transform", "rotate(-40)");
+  
+    svg.append("g").call(d3.axisLeft(y));
+  
+    for (let [key, value] of nestedData) {
+      svg.append("path").datum(value).attr("fill", "none").attr("stroke", color(key)).attr("stroke-width", 1.5).attr("d", line);
+    }
+}
